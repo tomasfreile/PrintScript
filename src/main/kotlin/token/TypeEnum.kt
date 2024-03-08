4989fce3d9146374c64e1208ed9eeeaec01d17e3
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-07 21:16:02 -0300

    More enums

--- src/main/kotlin/token/TypeEnum.kt
+++ src/main/kotlin/token/TypeEnum.kt
@@ -1,6 +1,16 @@
 package org.example.token
 
 enum class TypeEnum {
-    ASSIGNATION,
+    VARIABLE_KEYWORD, // VARIABLE_KEYWORD name: String
+    VALUE_IDENTIFIER, //let VALUE_IDENTIFIER: STRING
+    ASSIGNATION, // =
+    NUMBER_TYPE, //let name: NUMBER_TYPE
+    STRING_TYPE,
+    SUM, // +
+    SUBSTRACT, // -
+    PRODUCT,// *
+    DIVISION, // /
+    NUMBER,
+    STRING,
     TISTA
 }
