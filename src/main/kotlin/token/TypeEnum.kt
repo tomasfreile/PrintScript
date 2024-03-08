fbb9b026da8cbdf2ad6137426d41ab2c11abbb98
tomasfreile <freiletomy@gmail.com>
2024-03-08 11:42:00 -0300

    Re-organised enums

--- src/main/kotlin/token/TypeEnum.kt
+++ src/main/kotlin/token/TypeEnum.kt
@@ -1,26 +1,30 @@
 package org.example.token
 
 enum class TypeEnum {
-    // Single-character tokens.
-    ASSIGNATION, // =
-    LEFT_PAREN,
-    RIGHT_PAREN,
+
+    // Operators
     PLUS, // +
     MINUS, // -
     STAR,// *
     SLASH, // /
+    ASSIGNATION, // =
+
+    // Separators
     SEMICOLON, // ;
+    LEFT_PAREN, // (
+    RIGHT_PAREN, // )
+    COLON, // :
 
     // Keywords
     PRINT, // print
     IF, // if
     ELSE, // else
+    NUMBER_TYPE, // Number
+    STRING_TYPE, // String
+    VARIABLE_KEYWORD, // VARIABLE_KEYWORD name: String
 
     // Literals
     VALUE_IDENTIFIER, //let VALUE_IDENTIFIER: STRING
-    VARIABLE_KEYWORD, // VARIABLE_KEYWORD name: String
-    NUMBER_TYPE, // Number
-    STRING_TYPE, // String
     STRING, // "hola"
     NUMBER, // 123
 }
