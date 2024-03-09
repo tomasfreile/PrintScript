10f5f477a62d3d64ca2d251a6ce4e345e394d644
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-08 22:25:40 -0300

    Add Double Dot

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -1,10 +1,11 @@
-package kotlin
-
+import org.example.parser.Node
 import org.example.token.Coordinate
 import org.example.token.PrintScriptToken
 import org.example.token.Token
 import org.example.token.TypeEnum
 import org.junit.jupiter.api.Test
+import org.example.parser.Parser
+import org.junit.jupiter.api.Assertions.*
 
 class ParserTest {
 
@@ -13,8 +14,17 @@ class ParserTest {
         val tokenList: List<Token> = listOf(
             PrintScriptToken(TypeEnum.VARIABLE_KEYWORD, "let", Coordinate(2,3), Coordinate(2,3)),
             PrintScriptToken(TypeEnum.VALUE_IDENTIFIER, "name", Coordinate(2,3), Coordinate(2,3)),
-            PrintScriptToken(TypeEnum.)
+            PrintScriptToken(TypeEnum.DOUBLE_DOT, ":", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING_TYPE, "String", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.ASSIGNATION, "=", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING, "marcos", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.SEMICOLON, ";", Coordinate(2,3), Coordinate(2,3))
         )
+        val parser: Parser = Parser()
+        val node: Node? = parser.parse(tokenList)
+        assertNotNull(node)
+        assertEquals(node?.token?.type, TypeEnum.VARIABLE_KEYWORD)
+
     }
 
 }
