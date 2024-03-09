0946ce0602e26e5dfe32b05ae6ca3c745a2d7a7c
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-09 14:54:53 -0300

    Test

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -6,6 +6,7 @@ import org.example.token.TypeEnum
 import org.junit.jupiter.api.Test
 import org.example.parser.Parser
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.TestFactory
 
 class ParserTest {
 
@@ -45,4 +46,70 @@ class ParserTest {
         assertNotNull(node)
         assertEquals(node?.token?.type, TypeEnum.VARIABLE_KEYWORD)
     }
+
+    @Test
+    fun TripleSumDeclarationTest(){
+        val tokenList: List<Token> = listOf(
+            PrintScriptToken(TypeEnum.VARIABLE_KEYWORD, "let", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.VALUE_IDENTIFIER, "sum", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.DOUBLE_DOT, ":", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING_TYPE, "Int", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.ASSIGNATION, "=", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING, "3", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.PLUS, "+", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.NUMBER, "4", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.PLUS, "+", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.NUMBER, "5", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.SEMICOLON, ";", Coordinate(2,3), Coordinate(2,3))
+        )
+        val parser: Parser = Parser()
+        val node: Node? = parser.parse(tokenList)
+        assertNotNull(node)
+        assertEquals(node?.token?.type, TypeEnum.VARIABLE_KEYWORD)
+    }
+
+    @Test
+    fun BinaryProductTest(){
+        val tokenList: List<Token> = listOf(
+            PrintScriptToken(TypeEnum.VARIABLE_KEYWORD, "let", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.VALUE_IDENTIFIER, "product", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.DOUBLE_DOT, ":", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING_TYPE, "Int", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.ASSIGNATION, "=", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING, "3", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.PLUS, "*", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.NUMBER, "4", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.SEMICOLON, ";", Coordinate(2,3), Coordinate(2,3))
+        )
+        val parser: Parser = Parser()
+        val node: Node? = parser.parse(tokenList)
+        assertNotNull(node)
+        assertEquals(node?.token?.type, TypeEnum.VARIABLE_KEYWORD)
+    }
+
+    @Test
+    fun BinaryDivisionTest(){
+        val tokenList: List<Token> = listOf(
+            PrintScriptToken(TypeEnum.VARIABLE_KEYWORD, "let", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.VALUE_IDENTIFIER, "product", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.DOUBLE_DOT, ":", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING_TYPE, "Int", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.ASSIGNATION, "=", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.STRING, "3", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.PLUS, "/", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.NUMBER, "4", Coordinate(2,3), Coordinate(2,3)),
+            PrintScriptToken(TypeEnum.SEMICOLON, ";", Coordinate(2,3), Coordinate(2,3))
+        )
+        val parser: Parser = Parser()
+        val node: Node? = parser.parse(tokenList)
+        assertNotNull(node)
+        assertEquals(node?.token?.type, TypeEnum.VARIABLE_KEYWORD)
+    }
+
+    @Test
+    fun BinarySubstractTest(){
+
+    }
+
+
 }
