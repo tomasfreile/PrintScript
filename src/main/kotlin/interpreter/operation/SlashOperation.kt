65ad321cd323a5e29a5838392236c4f1b5add9d3
MaxiRG <maxiramirezgis@gmail.com>
2024-03-15 16:54:30 -0300

    Operations implemented, test modified

---
+++ src/main/kotlin/interpreter/operation/SlashOperation.kt
@@ -0,0 +1,12 @@
+package org.example.interpreter.operation
+
+class SlashOperation: Operation {
+    override val symbol = "/"
+
+    override fun resolve(l: Any?, r: Any?): Any? {
+        if(l is Int && r is Int){
+            return l / r
+        }
+        throw UnsupportedOperationException("Unsupported type for plus operation")
+    }
+}
--- src/test/kotlin/interpreter/PrintScriptInterpreterTest.kt
+++ src/test/kotlin/interpreter/PrintScriptInterpreterTest.kt
@@ -1,13 +1,16 @@
 package interpreter
 
-import org.example.interpreter.ParenInterpreter
-import org.example.interpreter.PrintInterpreter
-import org.example.interpreter.PrintScriptInterpreter
-import org.example.interpreter.StringInterpreter
+import org.example.interpreter.*
+import org.example.interpreter.operation.MinusOperation
+import org.example.interpreter.operation.PlusOperation
+import org.example.interpreter.operation.SlashOperation
+import org.example.interpreter.operation.StarOperation
 import org.example.parser.ASTSingleNode
 import org.example.token.Coordinate
 import org.example.token.PrintScriptToken
 import org.example.token.TypeEnum
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
@@ -15,6 +18,29 @@ import kotlin.test.assertEquals
 
 
 class PrintScriptInterpreterTest {
+
+    val operationInterpreter = OperationInterpreter(listOf(PlusOperation(), MinusOperation(), SlashOperation(), StarOperation()))
+
+    val interpreterMap =  mapOf(
+        Pair(TypeEnum.LEFT_PAREN, ParenInterpreter()),
+        Pair(TypeEnum.RIGHT_PAREN, ParenInterpreter()),
+        Pair(TypeEnum.STRING, StringInterpreter()),
+        Pair(TypeEnum.PRINT, PrintInterpreter()),
+        Pair(TypeEnum.NUMBER, NumberInterpreter()),
+        Pair(TypeEnum.STAR, operationInterpreter),
+        Pair(TypeEnum.SLASH, operationInterpreter),
+        Pair(TypeEnum.PLUS, operationInterpreter),
+        Pair(TypeEnum.MINUS, operationInterpreter),
+    )
+
+    private var printedCode = ""
+
+    fun resestSystemOut(){
+
+    }
+
+
+
     @Test
     fun testBasicPrint() {
         val ast =
@@ -28,22 +54,17 @@ class PrintScriptInterpreterTest {
                     PrintScriptToken(TypeEnum.LEFT_PAREN,"(", Coordinate(2, 3), Coordinate(2, 3))),
             PrintScriptToken(TypeEnum.PRINT, "println", Coordinate(2,3), Coordinate(2, 3)))
 
-        val interpreter = PrintScriptInterpreter(mapOf(
-            Pair(TypeEnum.LEFT_PAREN, ParenInterpreter()),
-            Pair(TypeEnum.RIGHT_PAREN, ParenInterpreter()),
-            Pair(TypeEnum.STRING, StringInterpreter()),
-            Pair(TypeEnum.PRINT, PrintInterpreter())
-        ));
+        val interpreter = PrintScriptInterpreter(interpreterMap);
+
         val outputStream = ByteArrayOutputStream()
         System.setOut(PrintStream(outputStream))
 
         interpreter.interpret(ast)
-        // Retrieve the printed content
+
         val printedCode = outputStream.toString().trim()
         System.setOut(System.out)
 
         assertEquals("a", printedCode)
-
     }
 
 }
