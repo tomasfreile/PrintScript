0b4782c83fecb5d6eb1f1f02f4f8f24916da0adb
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-21 15:54:36 -0300

    Parser fix, check invalid operators

--- parser/src/main/kotlin/parser/Parser.kt
+++ parser/src/main/kotlin/parser/Parser.kt
@@ -3,49 +3,28 @@ package parser
 import ast.Node
 import org.example.parser.PrintParse
 import token.Token
-import token.TokenType
 import org.example.parser.semantic.SemanticChecker
 import org.example.parser.sintactic.SintacticChecker
+import org.example.parser.sintactic.declarative.isDeclarative
+import parser.parse.AssignationParse
+import parser.parse.DeclarationParse
+import parser.sintactic.commons.HasInvalidOperator
+import parser.sintactic.isAssignation
+import parser.sintactic.isPrint
 
-data class InvalidTokenInput(override val message: String): Exception(message)
+data class InvalidSyntax(override val message: String): Exception(message)
 class Parser(
-    val syntaxRules: List<SintacticChecker>?,
-    val semanticRules: List<SemanticChecker>?
+    val syntaxRules: List<SintacticChecker>? = null,
+    val semanticRules: List<SemanticChecker>? = null
 ) {
-    fun parse(tokenList: List<Token>): Node?{
-        return if(tokenList.isNotEmpty() && hasGoodSyntax(tokenList) && hasGoodSemantic(tokenList)){
-            chooseParse(tokenList)
-        }else{
-            null
+    fun parse(tokenList: List<Token>): Node {
+        if(HasInvalidOperator().checkSyntax(tokenList)) throw InvalidSyntax("Has an Invalid Operator")
+        return when{
+            isDeclarative().checkSyntax(tokenList) -> DeclarationParse().parse(tokenList)
+            isPrint().checkSyntax(tokenList) -> PrintParse().parse(tokenList)
+            isAssignation().checkSyntax(tokenList) -> AssignationParse().parse(tokenList)
+            else ->  throw InvalidSyntax("Invalid TokenInput Syntax :(")
         }
     }
 
-    private fun chooseParse(tokenList: List<Token>): Node{
-        val token = tokenList.first()
-        when(token.type){
-            TokenType.VARIABLE_KEYWORD -> return DeclarationParse().parse(tokenList)
-            TokenType.PRINT -> return PrintParse().parse(tokenList)
-            else -> { throw InvalidTokenInput("Invalid TokenInput") }
-        }
-    }
-
-    private fun hasGoodSemantic(tokenList: List<Token>): Boolean{
-        if (semanticRules != null) {
-            for(rule in semanticRules){
-                if(rule.checkSemantic(tokenList)) continue
-                else return false
-            }
-        }
-        return true
-    }
-
-    private fun hasGoodSyntax(tokenList: List<Token>): Boolean{
-        if (syntaxRules != null) {
-            for(rule in syntaxRules){
-                if(rule.checkSyntax(tokenList)) continue
-                else return false
-            }
-        }
-        return true
-    }
 }
