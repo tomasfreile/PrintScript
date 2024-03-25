b80436a3c082208f406efb1b974aeebd8d2b62d5
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-25 17:59:43 -0300

    Assignation parse test

--- parser/src/main/kotlin/parser/parse/AssignationParse.kt
+++ parser/src/main/kotlin/parser/parse/AssignationParse.kt
@@ -1,63 +1,57 @@
 package parser.parse
 
-import ast.ASTBinaryNode
 import ast.ASTSingleNode
 import ast.Node
-import parser.sintactic.commons.HasCombination
+import parser.BinaryNodeBuilder
 import token.Token
-import token.TokenType as Type
+import token.TokenType
 
 class AssignationParse : Parse {
     override fun parse(tokenList: List<Token>): Node {
         val token = tokenList.first()
-        return when (token.type) {
-            Type.SEMICOLON -> ASTSingleNode(null, token)
-            Type.VALUE_IDENTIFIER -> {
-                if (HasCombination().checkSyntax(tokenList)) {
-                    ASTSingleNode(buildBinaryNode(tokenList.subList(1, tokenList.size)), token)
-                } else {
-                    ASTSingleNode(parse(tokenList.subList(1, tokenList.size)), token)
-                }
-            }
+        return when {
+            breakRecursion(token) -> ASTSingleNode(null, token)
+            isLiteral(token) -> handleLiteral(tokenList)
+            ignore(token) -> parse(tokenList.subList(1, tokenList.size))
             else -> ASTSingleNode(parse(tokenList.subList(1, tokenList.size)), token)
         }
     }
 
-    private fun buildBinaryNode(tokenList: List<Token>): Node {
-        val token = tokenList.first()
-        return when (token.type) {
-            Type.ASSIGNATION -> ASTSingleNode(buildBinaryNode(tokenList.subList(1, tokenList.size)), token)
-            Type.STRING, Type.NUMBER, Type.VALUE_IDENTIFIER -> handleLiteral(tokenList)
-            Type.SEMICOLON -> ASTSingleNode(null, token)
-            else -> ASTSingleNode(null, token)
-        }
-    }
-
-    private fun handleLiteral(tokenList: List<Token>): Node {
-        val plusIndex = getPlusIndex(tokenList)
-        return if (plusIndex != -1) {
-            ASTBinaryNode(
-                buildBinaryNode(tokenList.subList(plusIndex + 1, tokenList.size)),
-                buildBinaryNode(tokenList.subList(0, plusIndex)),
-                tokenList[plusIndex],
-            )
+    private fun handleLiteral(tokenList: List<Token>): Node { // if next is an operator
+        return if (tokenList.size == 1) {
+            ASTSingleNode(null, tokenList.first())
+        } else if (isOperator(tokenList[1])) {
+            BinaryNodeBuilder(PrintParse()).build(tokenList)
         } else {
-            if (tokenList.size == 1) {
-                ASTSingleNode(null, tokenList.first())
-            } else {
-                ASTSingleNode(parse(tokenList.subList(1, tokenList.size)), tokenList.first())
-            }
+            ASTSingleNode(parse(tokenList.subList(1, tokenList.size)), tokenList.first())
         }
     }
 
-    private fun getPlusIndex(tokenList: List<Token>): Int {
-        var index = 0
-        for (token in tokenList) {
-            when (token.type) {
-                Type.PLUS -> return index
-                else -> index += 1
-            }
+    private fun isLiteral(token: Token): Boolean {
+        return when (token.type) {
+            TokenType.STRING, TokenType.NUMBER, TokenType.VALUE_IDENTIFIER -> true
+            else -> false
         }
-        return -1
+    }
+
+    private fun isOperator(token: Token): Boolean {
+        return when (token.type) {
+            TokenType.PLUS, TokenType.MINUS, TokenType.STAR, TokenType.SLASH -> true
+            else -> false
+        }
+    }
+
+    private fun breakRecursion(token: Token): Boolean {
+        return when (token.type) {
+            TokenType.SEMICOLON -> true
+            else -> false
+        }
+    }
+
+    private fun ignore(token: Token): Boolean {
+        return when (token.type) {
+            TokenType.LEFT_PAREN, TokenType.RIGHT_PAREN -> true
+            else -> false
+        }
     }
 }
--- parser/src/test/kotlin/parser/parse/AssignationParseTest.kt
+++ parser/src/test/kotlin/parser/parse/AssignationParseTest.kt
@@ -73,4 +73,46 @@ class AssignationParseTest {
             assertTrue(true)
         }
     }
+
+    @Test
+    fun parseConcatenationAssignationTest() {
+        val tokenList: List<Token> =
+            listOf(
+                PrintScriptToken(TokenType.VALUE_IDENTIFIER, "a", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.ASSIGNATION, "=", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER, "3", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.PLUS, "+", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STRING, " holas", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.SEMICOLON, ";", Coordinate(2, 3), Coordinate(2, 3)),
+            )
+        val node: Node
+        try {
+            node = parser.parse(tokenList)
+            assertTrue(true)
+            println(node)
+        } catch (e: InvalidSyntax) {
+            assertTrue(false)
+        }
+    }
+
+    @Test
+    fun parseInvalidConcatenationAssignationTest() {
+        val tokenList: List<Token> =
+            listOf(
+                PrintScriptToken(TokenType.VALUE_IDENTIFIER, "a", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.ASSIGNATION, "=", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER, "3", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STAR, "*", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STRING, " holas", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.SEMICOLON, ";", Coordinate(2, 3), Coordinate(2, 3)),
+            )
+        val node: Node
+        try {
+            node = parser.parse(tokenList)
+            assertTrue(false)
+            println(node)
+        } catch (e: InvalidSyntax) {
+            assertTrue(true)
+        }
+    }
 }
