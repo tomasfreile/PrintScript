89e1549162f542d69be2ed5865ff2f6ec71920f9
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-31 23:04:22 -0300

    Parse main algorithm

--- parser/src/main/kotlin/parser/Parser.kt
+++ parser/src/main/kotlin/parser/Parser.kt
@@ -2,13 +2,13 @@ package parser
 
 import ast.Node
 import parser.parse.AssignationParse
-import parser.parse.DeclarationParse
 import parser.parse.PrintParse
 import parser.sintactic.IsAssignation
 import parser.sintactic.IsDeclarative
 import parser.sintactic.IsPrint
 import parser.sintactic.commons.HasInvalidOperator
 import parser.sintactic.commons.HasString
+import parser.sintactic.commons.HasValidParen
 import parser.sintactic.commons.IsConcatenation
 import token.Token
 
@@ -18,9 +18,8 @@ class Parser {
     fun parse(tokenList: List<Token>): Node {
         preConditions(tokenList)
         return when {
-            IsDeclarative().checkSyntax(tokenList) -> DeclarationParse().parse(tokenList)
+            IsDeclarative().checkSyntax(tokenList) || IsAssignation().checkSyntax(tokenList) -> AssignationParse().parse(tokenList)
             IsPrint().checkSyntax(tokenList) -> PrintParse().parse(tokenList)
-            IsAssignation().checkSyntax(tokenList) -> AssignationParse().parse(tokenList)
             else -> throw InvalidSyntax("Invalid TokenInput Syntax :(")
         }
     }
@@ -28,5 +27,6 @@ class Parser {
     private fun preConditions(tokenList: List<Token>) {
         if (HasInvalidOperator().checkSyntax(tokenList)) throw InvalidSyntax("Has an Invalid Operator")
         if (!IsConcatenation().checkSyntax(tokenList) && HasString().checkSyntax(tokenList)) throw InvalidSyntax("String just concatenate")
+        if (!HasValidParen().checkSyntax(tokenList)) throw InvalidSyntax("Has Invalid Paren")
     }
 }
