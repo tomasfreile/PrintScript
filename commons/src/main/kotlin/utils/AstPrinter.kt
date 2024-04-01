4d316d58a709d60d1da077cea443425fc8d0eed9
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-03-31 21:49:21 -0300

    printer refactor

--- commons/src/main/kotlin/utils/AstPrinter.kt
+++ commons/src/main/kotlin/utils/AstPrinter.kt
@@ -17,10 +17,10 @@ fun printAST(
     when (node) {
         is ASTBinaryNode -> {
             println("$indentation ${underline}Binary Node:$reset ${node.token.value}")
-            println("$indentation ${underline}Right:$reset")
-            printAST(node.right, depth + 1)
             println("$indentation ${underline}Left:$reset")
             printAST(node.left, depth + 1)
+            println("$indentation ${underline}Right:$reset")
+            printAST(node.right, depth + 1)
         }
         is ASTSingleNode -> {
             println("$indentation Single Node: ${node.token.value}")
