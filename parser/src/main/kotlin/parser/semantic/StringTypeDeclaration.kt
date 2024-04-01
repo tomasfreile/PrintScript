8502a46e69def1ba164ab2372e69b29b32a1057c
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-04-01 10:52:05 -0300

    rm useless files

--- parser/src/main/kotlin/parser/semantic/StringTypeDeclaration.kt
+++
@@ -1,9 +0,0 @@
-package parser.semantic
-
-import token.Token
-
-class StringTypeDeclaration : SemanticChecker {
-    override fun checkSemantic(tokenList: List<Token>): Boolean {
-        TODO("Not yet implemented")
-    }
-}
