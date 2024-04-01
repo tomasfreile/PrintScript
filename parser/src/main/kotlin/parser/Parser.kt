d9c366796e05cbb79da6641755c635476ba73746
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-04-01 12:41:52 -0300

    add semantic checkers logic

--- parser/src/main/kotlin/parser/Parser.kt
+++ parser/src/main/kotlin/parser/Parser.kt
@@ -3,6 +3,9 @@ package parser
 import ast.Node
 import parser.parse.AssignationParse
 import parser.parse.PrintParse
+import parser.semantic.NumberTypeAssignationContent
+import parser.semantic.PrintContent
+import parser.semantic.StringTypeAssignationContent
 import parser.sintactic.IsAssignation
 import parser.sintactic.IsDeclarative
 import parser.sintactic.IsPrint
@@ -11,22 +14,49 @@ import parser.sintactic.commons.HasString
 import parser.sintactic.commons.HasValidParen
 import parser.sintactic.commons.IsConcatenation
 import token.Token
+import token.TokenType
 
 data class InvalidSyntax(override val message: String) : Exception(message)
 
+data class InvalidCommonSense(override val message: String) : Exception(message)
+
 class Parser {
     fun parse(tokenList: List<Token>): Node {
-        preConditions(tokenList)
+        checkSyntax(tokenList)
         return when {
-            IsDeclarative().checkSyntax(tokenList) || IsAssignation().checkSyntax(tokenList) -> AssignationParse().parse(tokenList)
-            IsPrint().checkSyntax(tokenList) -> PrintParse().parse(tokenList)
-            else -> throw InvalidSyntax("Invalid TokenInput Syntax :(")
+            IsDeclarative().checkSyntax(tokenList) -> {
+                checkDeclaration(tokenList)
+                AssignationParse().parse(tokenList)
+            }
+            IsPrint().checkSyntax(tokenList) -> {
+                checkPrintSemantic(tokenList)
+                PrintParse().parse(tokenList)
+            }
+            IsAssignation().checkSyntax(tokenList) -> AssignationParse().parse(tokenList)
+            else -> throw InvalidCommonSense("")
         }
     }
 
-    private fun preConditions(tokenList: List<Token>) {
+    private fun checkSyntax(tokenList: List<Token>) {
         if (HasInvalidOperator().checkSyntax(tokenList)) throw InvalidSyntax("Has an Invalid Operator")
         if (!IsConcatenation().checkSyntax(tokenList) && HasString().checkSyntax(tokenList)) throw InvalidSyntax("String just concatenate")
         if (!HasValidParen().checkSyntax(tokenList)) throw InvalidSyntax("Has Invalid Paren")
+    }
+
+    private fun checkPrintSemantic(tokenList: List<Token>) {
+        if (!PrintContent().checkSemantic(tokenList)) throw InvalidCommonSense("Invalid Print Content, should be String or Number")
+    }
+
+    private fun checkDeclaration(tokenList: List<Token>) {
+        val dataType = tokenList[3]
+        when (dataType.type) {
+            TokenType.STRING_TYPE -> {
+                if (StringTypeAssignationContent().checkSemantic(tokenList)) throw InvalidCommonSense("String type has strings")
+            }
+            TokenType.NUMBER_TYPE -> {
+                if (NumberTypeAssignationContent().checkSemantic(tokenList)) throw InvalidCommonSense("Number type has numbers")
+            }
+            else -> return
+        }
     }
 }
