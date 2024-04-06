2096abbb41bdf6544f911554becd6b5b4839347b
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-04-06 16:00:10 -0300

    test

--- parser/src/test/kotlin/parser/DeclarationParserTest.kt
+++ parser/src/test/kotlin/parser/DeclarationParserTest.kt
@@ -1,11 +1,15 @@
 package parser
 
+import ast.BinaryOperationNode
+import ast.LiteralNode
+import ast.VariableDeclarationNode
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import parser.parse.DeclarationParser
 import token.Coordinate
 import token.PrintScriptToken
 import token.TokenType
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class DeclarationParserTest {
@@ -62,5 +66,61 @@ class DeclarationParserTest {
         assertThrows<InvalidDeclarationStatement> {
             parser.createAST(tokenList)
         }
+    }
+
+    @Test
+    fun test004_DeclarationParserCanHandleNumberDeclaration() {
+        val tokenList =
+            listOf(
+                PrintScriptToken(TokenType.LET, "let", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.VALUE_IDENTIFIER_LITERAL, "name", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.COLON, ":", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER_TYPE, "Int", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.ASSIGNATION, "=", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER_LITERAL, "3", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.SEMICOLON, ";", Coordinate(2, 3), Coordinate(2, 3)),
+            )
+        assertTrue(parser.canHandle(tokenList))
+    }
+
+    @Test
+    fun test005_DeclarationParserCanHandleConst() {
+        val tokenList =
+            listOf(
+                PrintScriptToken(TokenType.CONST, "const", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.VALUE_IDENTIFIER_LITERAL, "name", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.COLON, ":", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER_TYPE, "Int", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.ASSIGNATION, "=", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.NUMBER_LITERAL, "3", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.SEMICOLON, ";", Coordinate(2, 3), Coordinate(2, 3)),
+            )
+        assertTrue(parser.canHandle(tokenList))
+    }
+
+    @Test
+    fun test006_DeclarationParserStringConcat() {
+        val tokenList =
+            listOf(
+                PrintScriptToken(TokenType.LET, "let", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.VALUE_IDENTIFIER_LITERAL, "name", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.COLON, ":", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STRING_TYPE, "String", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.ASSIGNATION, "=", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STRING_LITERAL, "Tatiana", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.PLUS, "+", Coordinate(2, 3), Coordinate(2, 3)),
+                PrintScriptToken(TokenType.STRING_LITERAL, "Malano", Coordinate(2, 3), Coordinate(2, 3)),
+            )
+        assertTrue { parser.canHandle(tokenList) }
+        val node = parser.createAST(tokenList)
+        assertTrue {
+            node is VariableDeclarationNode
+            (node as VariableDeclarationNode).expression is BinaryOperationNode
+        }
+        assertEquals((((node as VariableDeclarationNode).expression as BinaryOperationNode).left as LiteralNode).value, "Tatiana")
+    }
+
+    @Test
+    fun test007_DeclarationParserSimpleArithmeticExpression() {
     }
 }
