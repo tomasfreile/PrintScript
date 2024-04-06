a10718d03f21b9515779fffa8c33cf7679c7a645
marcos <marcos.lambertini@ing.austral.edu.ar>
2024-04-05 23:39:11 -0300

    Arithmetic node builder algorithm with paren feature

--- parser/src/main/kotlin/parser/nodeBuilder/ArithmeticNodeBuilder.kt
+++ parser/src/main/kotlin/parser/nodeBuilder/ArithmeticNodeBuilder.kt
@@ -2,17 +2,38 @@ package parser.nodeBuilder
 
 import ast.AstNode
 import ast.BinaryOperationNode
+import parser.InvalidSyntaxException
+import parser.analysis.sintactic.HasOperationAfterParen
+import parser.analysis.sintactic.HasPairOfParen
 import token.Token
 import token.TokenType
 
 class ArithmeticNodeBuilder : NodeBuilder {
     override fun build(tokenList: List<Token>): AstNode {
         return when {
-            isSplit(tokenList) -> split(tokenList)
-            isLiteral(tokenList.first()) -> createLiteralNode(tokenList)
-            isOtherOperator(tokenList[1]) -> createBinaryNode(tokenList, 1) // L O L format
+            isSplit(tokenList) -> split(tokenList) // Primero separo en términos
+            isLeftParen(tokenList.first()) -> handleLeftParen(tokenList) // Resuelvo paréntesis
+            isBinaryOperation(tokenList) -> createBinaryNode(tokenList, 1) // L O L format Resuelvo producto y división
+            isLiteral(tokenList.first()) -> createLiteralNode(tokenList) // llegué al final..
             else -> createLiteralNode(tokenList)
         }
+    }
+
+    private fun handleLeftParen(tokenList: List<Token>): AstNode {
+        val rightParenIndex = getRightParenIndex(tokenList)
+        return if (hasOperator(tokenList)) {
+            BinaryOperationNode(
+                build(tokenList.subList(1, rightParenIndex)),
+                build(tokenList.subList(rightParenIndex + 2, tokenList.size)),
+                tokenList[rightParenIndex + 1].type,
+            )
+        } else {
+            build(tokenList.subList(1, rightParenIndex))
+        }
+    }
+
+    private fun hasOperator(tokenList: List<Token>): Boolean {
+        return HasOperationAfterParen().checkSyntax(tokenList)
     }
 
     private fun createBinaryNode(
@@ -51,9 +72,10 @@ class ArithmeticNodeBuilder : NodeBuilder {
         type: TokenType,
     ): Int {
         var i = 0
-        for (token in tokenList) {
-            when (token.type) {
-                TokenType.PLUS, TokenType.MINUS -> return i
+        while (i < tokenList.size) {
+            when (tokenList[i].type) {
+                TokenType.LEFT_PAREN -> i = ignoreParenToIndex(tokenList, i)
+                type -> return i
                 else -> i++
             }
         }
@@ -67,11 +89,14 @@ class ArithmeticNodeBuilder : NodeBuilder {
         return observedIndex > 0 && (otherIndex < 0 || observedIndex < otherIndex)
     }
 
-    private fun isOtherOperator(token: Token): Boolean {
-        return when (token.type) {
-            TokenType.STAR, TokenType.SLASH -> true
-            else -> false
+    private fun isBinaryOperation(tokenList: List<Token>): Boolean {
+        if (tokenList.size == 3) {
+            return when (tokenList[1].type) {
+                TokenType.STAR, TokenType.SLASH -> isLiteral(tokenList[0]) && isLiteral(tokenList[2])
+                else -> false
+            }
         }
+        return false
     }
 
     private fun isLiteral(token: Token): Boolean {
@@ -79,5 +104,41 @@ class ArithmeticNodeBuilder : NodeBuilder {
             TokenType.NUMBER_LITERAL, TokenType.VALUE_IDENTIFIER_LITERAL -> true
             else -> false
         }
+    }
+
+    private fun isLeftParen(token: Token): Boolean {
+        return when (token.type) {
+            TokenType.LEFT_PAREN -> true
+            else -> false
+        }
+    }
+
+    private fun isParenValid(
+        tokenList: List<Token>,
+        from: Int,
+    ): Boolean {
+        return HasPairOfParen().checkSyntax(tokenList.subList(from, tokenList.size))
+    }
+
+    private fun ignoreParenToIndex(
+        tokenList: List<Token>,
+        actual: Int,
+    ): Int {
+        if (isParenValid(tokenList, actual)) {
+            return actual + getOperatorIndex(tokenList.subList(actual + 1, tokenList.size), TokenType.RIGHT_PAREN) + 2
+        } else {
+            throw InvalidSyntaxException("Invalid Paren Syntax on line: " + tokenList[actual].start.row)
+        }
+    }
+
+    private fun getRightParenIndex(tokenList: List<Token>): Int {
+        var i = 0
+        for (token in tokenList) {
+            when (token.type) {
+                TokenType.RIGHT_PAREN -> return i
+                else -> i += 1
+            }
+        }
+        return -1
     }
 }
