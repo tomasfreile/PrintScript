f500500a91c424af7c4af6e2b5f751eba6c17e6e
M4rkos2002 <marcos.lambertini@ing.austral.edu.ar>
2024-04-11 11:50:28 -0300

    modify semantic

--- parser/src/main/kotlin/parser/analysis/semantic/BooleanSemantic.kt
+++ parser/src/main/kotlin/parser/analysis/semantic/BooleanSemantic.kt
@@ -5,21 +5,13 @@ import token.TokenType
 
 class BooleanSemantic : SemanticRule {
     override fun checkSemantic(tokenList: List<Token>): Boolean {
-        return tokenList.size == 1 && checkValue(tokenList.first())
-    }
-
-    private fun checkValue(token: Token): Boolean {
-        return if (isLiteral(token)) {
-            when (token.value) {
-                "True", "False" -> true
-                else -> false
-            }
-        } else { // if is not boolean it is a variable
-            true
-        }
+        return tokenList.size == 1 && isLiteral(tokenList.first())
     }
 
     private fun isLiteral(token: Token): Boolean {
-        return token.type == TokenType.BOOLEAN_LITERAL
+        return when (token.type) {
+            TokenType.BOOLEAN_LITERAL, TokenType.VALUE_IDENTIFIER_LITERAL -> true
+            else -> false
+        }
     }
 }
