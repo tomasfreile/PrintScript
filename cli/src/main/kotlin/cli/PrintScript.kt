7a6a16ac7615257845c1d9301df55799f1eb1a45
tomasfreile <freiletomy@gmail.com>
2024-04-14 20:10:18 -0300

    added version w

--- cli/src/main/kotlin/cli/PrintScript.kt
+++ cli/src/main/kotlin/cli/PrintScript.kt
@@ -2,10 +2,10 @@ package cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.validate
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.file
-import formatter.Formatter
 import formatter.PrintScriptFormatter
 import interpreter.PrintScriptInterpreter
 import interpreter.builder.InterpreterBuilder
@@ -18,20 +18,44 @@ import lexer.factory.LexerBuilder
 import parser.PrintScriptParser
 import parser.parserBuilder.PrintScriptOnePointOneParserBuilder
 import parser.parserBuilder.PrintScriptOnePointZeroParserBuilder
-import sca.StaticCodeAnalyzer
 import sca.StaticCodeAnalyzerImpl
 import java.io.BufferedReader
 import java.io.File
 
-class PrintScript : CliktCommand(help = "PrintScript <Operation> <Source> <Version> <Config>") {
+class PrintScript : CliktCommand(help = "PrintScript <Version> <Operation> <Source> <Config>") {
+    private val version by argument(help = "a version")
+        .choice("1.0", "1.1")
+        .validate { require(it == "1.0" || it == "1.1") { "Invalid version: $it. Supported versions are 1.0 and 1.1" } }
+
     private val operation by argument(help = "the operation to apply to the file: validation, execution, formatting & analyzing")
         .choice("validate", "execute", "format", "analyze")
+
     private val source by argument(help = "source file").file(mustExist = true)
-    private val version by argument("-v", help = "a version")
+
     private val config by option("-c", help = "config file").file(mustExist = true)
 
-    val lexer: Lexer = LexerBuilder().build(version)
-    val parser: PrintScriptParser = buildParser(version)
+    private lateinit var lexer: Lexer
+    private lateinit var parser: PrintScriptParser
+    private lateinit var interpreter: PrintScriptInterpreter
+    private val symbolTable: MutableMap<Variable, Any> = mutableMapOf()
+
+    override fun run() {
+        lexer = LexerBuilder().build(version)
+        parser = buildParser(version)
+        interpreter = InterpreterBuilder().build()
+
+        val sentencesList = getSentenceList()
+        try {
+            when (operation) {
+                "execute" -> executeCode(sentencesList)
+                "format" -> formatCode(sentencesList)
+                "analyze" -> analyzeCode(sentencesList)
+                "validate" -> validateCode(sentencesList)
+            }
+        } catch (ex: Exception) {
+            println(ex.message)
+        }
+    }
 
     private fun buildParser(version: String): PrintScriptParser {
         return when (version) {
@@ -41,38 +65,10 @@ class PrintScript : CliktCommand(help = "PrintScript <Operation> <Source> <Versi
         }
     }
 
-    var interpreter: PrintScriptInterpreter = InterpreterBuilder().build()
-    val symbolTable: MutableMap<Variable, Any> = mutableMapOf()
-
-    override fun run() {
-        val sentencesList = getSentenceList()
-        try {
-            when (operation) {
-                "execute" -> {
-                    executeCode(sentencesList)
-                }
-
-                "format" -> {
-                    formatCode(sentencesList)
-                }
-
-                "analyze" -> {
-                    analyzeCode(sentencesList)
-                }
-
-                "validate" -> {
-                    validateCode(sentencesList)
-                }
-            }
-        } catch (ex: Exception) {
-            println(ex.message)
-        }
-    }
-
     private fun validateCode(sentencesList: List<String>) {
         for (sentence in sentencesList) {
             val tokenList = lexer.lex(sentence)
-            val tree = parser.createAST(tokenList)
+            parser.createAST(tokenList)
         }
         println("Validation successful!")
     }
@@ -86,16 +82,14 @@ class PrintScript : CliktCommand(help = "PrintScript <Operation> <Source> <Versi
         }
         when (result) {
             is PrintResult -> println(result.toPrint)
-            is Result -> Unit // If the result is not a print do nothing.
+            is Result -> Unit // If the result is not a print, do nothing.
         }
     }
 
     private fun formatCode(sentencesList: List<String>) {
-        val formatter: Formatter =
-            PrintScriptFormatter(config?.path ?: throw NullPointerException("Expected config file path for formatter."))
+        val formatter = PrintScriptFormatter(requireNotNull(config?.path) { "Expected config file path for formatter." })
         val file = File(source.path)
         var text = ""
-
         for (sentence in sentencesList) {
             val tokenList = lexer.lex(sentence)
             val tree = parser.createAST(tokenList)
@@ -106,17 +100,14 @@ class PrintScript : CliktCommand(help = "PrintScript <Operation> <Source> <Versi
     }
 
     private fun analyzeCode(sentencesList: List<String>) {
-        val sca: StaticCodeAnalyzer =
-            StaticCodeAnalyzerImpl(config?.path ?: throw NullPointerException("Expected config file path for sca."))
-        var errorList: List<String> = listOf()
+        val sca = StaticCodeAnalyzerImpl(requireNotNull(config?.path) { "Expected config file path for sca." })
+        var errorList = emptyList<String>()
         for (sentence in sentencesList) {
             val tokenList = lexer.lex(sentence)
             val tree = parser.createAST(tokenList)
-            errorList = errorList.plus(sca.analyze(tree ?: throw NullPointerException("Null ast tree")))
+            errorList += sca.analyze(tree ?: throw NullPointerException("Null AST tree"))
         }
-        for (error in errorList) {
-            println(error)
-        }
+        errorList.forEach(::println)
     }
 
     private fun getSentenceList(): List<String> {
