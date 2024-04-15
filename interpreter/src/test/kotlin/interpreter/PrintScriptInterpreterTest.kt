58b4706e66e55018a5fb089b28c3303773770c4f
tomasfreile <freiletomy@gmail.com>
2024-04-15 13:46:06 -0300

    fix: read_input

--- interpreter/src/test/kotlin/interpreter/PrintScriptInterpreterTest.kt
+++ interpreter/src/test/kotlin/interpreter/PrintScriptInterpreterTest.kt
@@ -152,6 +152,15 @@ class PrintScriptInterpreterTest {
     }
 
     @Test
+    fun testReadInputCanBePreAssigned() {
+        symbolTable[Variable("input", TokenType.STRING_TYPE, TokenType.LET)] = "hola"
+        val tokenPosition = TokenPosition(Coordinate(1, 0), Coordinate(1, 0))
+        val tree = FunctionNode(TokenType.READ_INPUT, LiteralNode("Write a number", TokenType.STRING_LITERAL, tokenPosition), tokenPosition)
+        val result = interpreter.interpret(tree, symbolTable)
+        assertEquals("hola", result)
+    }
+
+    @Test
     fun testBooleanLiteralReturn() {
         val string = "let bool: boolean = true;"
         val result = interpreter.interpret(getTree(string), symbolTable) as Result
